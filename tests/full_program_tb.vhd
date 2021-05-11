3b14a46edc5c7d480a38fb17c67c444234e90c4e
Will Werst <werstiv@gmail.com>
2021-05-11 00:22:18 -0700

    Add program loading

--- tests/full_program_tb.vhd
+++ tests/full_program_tb.vhd
@@ -17,8 +17,8 @@ use ieee.std_logic_textio.all;
 
 entity full_program_tb is
     generic (
-        program_file    : string;
-        scratchpad_file : string
+        program_filename    : string;
+        scratchpad_filename : string
     );
 end full_program_tb;
 
@@ -28,6 +28,21 @@ architecture testbench of full_program_tb is
     constant CLK_PERIOD : time := 1 ms;
     signal done : boolean := FALSE;
     signal clk : std_logic := '0';
+
+    type prog_inst_arr_t is array (0 to 255) of std_logic_vector(63 downto 0);
+
+    type scratchpad_arr_t is array (0 to (2097152/8)-1) of std_logic_vector(63 downto 0);
+
+    signal ProgInstructions : prog_inst_arr_t;
+    signal Scratchpad       : scratchpad_arr_t;
+
+    function to_hex(slv : std_logic_vector) return string is
+        variable l : line;
+    begin
+        hwrite(l, slv);
+        return l.all;
+    end to_hex;
+
 begin
     CLOCK_PROC: process begin
         while not done loop
@@ -40,10 +55,32 @@ begin
     end process CLOCK_PROC;
 
     TEST_PROC: process
+        file program_file    : text is program_filename;
+        file scratchpad_file : text is scratchpad_filename;
+        variable linenum     : integer := 0;
+        variable line_buf: line;
+        variable long_std_vec: std_logic_vector(63 downto 0);
     begin
+        -- Load the program data
+        linenum := 0;
+        while not endfile(program_file) loop
+            readline(program_file, line_buf);
+            hread(line_buf, long_std_vec);
+            ProgInstructions(linenum) <= long_std_vec;
+            linenum := linenum + 1;
+        end loop;
+        -- Load the scratchpad data
+        linenum := 0;
+        while not endfile(scratchpad_file) loop
+            readline(scratchpad_file, line_buf);
+            hread(line_buf, long_std_vec);
+            Scratchpad(linenum) <= long_std_vec;
+            linenum := linenum + 1;
+        end loop;
         wait for 100 ms;
-        report program_file;
-        report scratchpad_file; 
+        for linenum in ProgInstructions'range loop
+            report to_hex(ProgInstructions(linenum));
+        end loop;
         done <= TRUE;
         wait;
     end process;
--- tests/program.py
+++ tests/program.py
@@ -3,6 +3,12 @@
 
 import ctypes
 import _ctypes
+import mmap
+
+
+def uint_to_hexline(val):
+    assert isinstance(val, ctypes.c_uint64)
+    return f"{val.value:016X}\n"
 
 
 class GHDLProgramExecutor(object):
@@ -10,12 +16,22 @@ class GHDLProgramExecutor(object):
     def __init__(self, ghdl_bin_path='bin/full_program_tb'):
         self.ghdl_bin_path = ghdl_bin_path
 
-    def call_ghdl(self, program_file, scratchpad_file):
+    def call_ghdl(self, program_data, scratchpad_data):
+        program_filename = 'program_data.hex'
+        scratchpad_filename = 'scratchpad_init_data.hex'
+        with open(program_filename, 'w+') as program_file:
+            # Each program instruction is a line
+            program_file.writelines(
+                [uint_to_hexline(prog_inst) for prog_inst in program_data])
+        with open(scratchpad_filename, 'w+') as scratchpad_file:
+            # Each 8-byte section of scratchpad is a line
+            scratchpad_file.writelines(
+                [uint_to_hexline(word) for word in scratchpad_data])
         try:
             ghdl_bin = ctypes.CDLL(self.ghdl_bin_path)
             args = [
-                f'-gPROGRAM_FILE="{program_file}"',
-                f'-gSCRATCHPAD_FILE="{scratchpad_file}"']
+                f'-gPROGRAM_FILENAME={program_filename}',
+                f'-gSCRATCHPAD_FILENAME={scratchpad_filename}']
             xargs = (ctypes.POINTER(ctypes.c_char) * (len(args)+1))()
             for idx, arg in enumerate(args):
                 xargs[idx+1] = ctypes.create_string_buffer(arg.encode('utf-8'))
@@ -26,8 +42,14 @@ class GHDLProgramExecutor(object):
 
 
 def main():
+    program_length = 256
+    scratchpad_length = 2097152 // 8
     program_executor = GHDLProgramExecutor()
-    program_executor.call_ghdl('program_file_path', 'scratchpad_file_path')
+    program_data = [ctypes.c_uint64(i) for i in range(program_length)]
+    scratchpad_data = [ctypes.c_uint64(scratchpad_length-i-1) for i in range(scratchpad_length)]
+    program_executor.call_ghdl(
+        program_data,
+        scratchpad_data)
 
 
 if __name__ == '__main__':
