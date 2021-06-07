171c4177e7d84962a2d70b8c421463a2a9eca7da
Will Werst <werstiv@gmail.com>
2021-06-07 09:47:20 -0700

    Hack fix tests using too much memory

--- src/randx_cfg.vhd
+++ src/randx_cfg.vhd
@@ -18,11 +18,11 @@ package RdxCfg is
     constant RANDOMX_PROGRAM_SIZE : natural := 256;
     constant RANDOMX_PROGRAM_ITERATIONS : natural := 2048;
     constant RANDOMX_PROGRAM_COUNT : natural := 8;
-    constant RANDOMX_SCRATCHPAD_L3 : natural := 2097152;
+    constant RANDOMX_SCRATCHPAD_L3 : natural := 16384; -- 2097152;
     constant RANDOMX_SCRATCHPAD_L2 : natural := 262144;
     constant RANDOMX_SCRATCHPAD_L1 : natural := 16384;
 
-    constant LOG2_RANDOMX_SCRATCHPAD_L3 : natural := 21;
+    constant LOG2_RANDOMX_SCRATCHPAD_L3 : natural := 14; -- 21;
 
 
 end package;
