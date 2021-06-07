2e40381215727de25631d07d9ba24ae0eace67e8
Will Werst <werstiv@gmail.com>
2021-06-07 09:35:43 -0700

    Make L3 scratchpad size original size

--- src/randx_cfg.vhd
+++ src/randx_cfg.vhd
@@ -18,7 +18,7 @@ package RdxCfg is
     constant RANDOMX_PROGRAM_SIZE : natural := 256;
     constant RANDOMX_PROGRAM_ITERATIONS : natural := 2048;
     constant RANDOMX_PROGRAM_COUNT : natural := 8;
-    constant RANDOMX_SCRATCHPAD_L3 : natural := 262144; -- 2097152;
+    constant RANDOMX_SCRATCHPAD_L3 : natural := 2097152;
     constant RANDOMX_SCRATCHPAD_L2 : natural := 262144;
     constant RANDOMX_SCRATCHPAD_L1 : natural := 16384;
 
